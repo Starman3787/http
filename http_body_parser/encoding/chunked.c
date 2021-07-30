7da8e01aa72d47cc03eaa2f66b47689982359a1e
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-30 18:09:38 +0100

    add function to handle http body with the transfer-encoding header set to "chunked"

---
+++ http_body_parser/encoding/chunked.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../util/hex_to_int/hex_to_int.c"
+
+char *chunked(char *httpNoHeaders, size_t *outputLength)
+{
+    char *output = malloc(sizeof(char) * 1);
+    *output = '\0';
+    *outputLength = 1;
+    while (1)
+    {
+        char *rawChunkLength = malloc(sizeof(char) * 1);
+        *rawChunkLength = '\0';
+        for (uint8_t i = 1; !(*httpNoHeaders == '\r' && *(httpNoHeaders + 1) == '\n'); i++, httpNoHeaders++)
+        {
+            rawChunkLength = realloc(rawChunkLength, sizeof(char) * (i + 1));
+            *(rawChunkLength + i - 1) = *httpNoHeaders;
+            *(rawChunkLength + i) = '\0';
+        }
+        uint32_t chunkLength = hex_to_int(rawChunkLength);
+        free(rawChunkLength);
+        if (chunkLength == 0)
+            break;
+        httpNoHeaders += 2;
+        (*outputLength) += chunkLength;
+        output = realloc(output, *outputLength);
+        strncpy(output, httpNoHeaders, chunkLength);
+        *(output + (*outputLength) - 1) = '\0';
+        httpNoHeaders += chunkLength + 2;
+    }
+    return output;
+}

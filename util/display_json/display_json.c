befa9b27eea3fc315205754253e942ab98ec77af
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-02 00:52:08 +0100

    simple function to show the json which has been parsed

---
+++ util/display_json/display_json.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "../../http_body_parser/http_body_structures.h"
+
+void display_json(Json **jsonBody, const size_t child_size)
+{
+    uint16_t child_elements = child_size / sizeof(Json *);
+    printf("%d\n", child_elements);
+    for (uint16_t i = 0; i < child_elements; i++)
+    {
+        switch ((*(jsonBody + i))->type)
+        {
+        case JSON_NUMBER:
+            printf("%s: %d\n", (*(jsonBody + i))->key, (*(jsonBody + i))->data.json_number);
+            break;
+        case JSON_STRING:
+            printf("%s: \"%s\"\n", (*(jsonBody + i))->key, (*(jsonBody + i))->data.json_string);
+            break;
+        case JSON_BOOLEAN:
+            char *displayBoolean = (*(jsonBody + i))->data.json_boolean ? "true" : "false";
+            printf("%s: %s\n", (*(jsonBody + i))->key, displayBoolean);
+            break;
+        case JSON_ARRAY:
+            if ((*(jsonBody + i))->key)
+                printf("%s:", (*(jsonBody + i))->key);
+            puts("[");
+            display_json((*(jsonBody + i))->data.json_array, (*(jsonBody + i))->child_size);
+            puts("]");
+            break;
+        case JSON_OBJECT:
+            if ((*(jsonBody + i))->key)
+                printf("%s:", (*(jsonBody + i))->key);
+            puts("{");
+            display_json((*(jsonBody + i))->data.json_object, (*(jsonBody + i))->child_size);
+            puts("}");
+            break;
+        }
+    }
+}

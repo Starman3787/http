3247991df4229a0dfe0aa3993e08caf54d9db757
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-03 02:09:03 +0100

    function to display the headers

---
+++ util/display_headers/display_headers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "../../http_header_parser/http_header_structures.h"
+
+void display_headers(Header **headers, uint8_t headersLength)
+{
+    for (uint8_t i = 0; i < headersLength; i++)
+        printf("%s: %s\n", (*(headers + i))->key, (*(headers + i))->value);
+}

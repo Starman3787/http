eb76cc27e50be879c65348fa97602f77c24416ac
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-15 14:06:53 +0100

    correct lower casing

--- util/find_header/find_header.c
+++ util/find_header/find_header.c
@@ -1,15 +1,16 @@
 #ifndef FIND_HEADER_C
 #define FIND_HEADER_C
 
-#include <ctype.h>
 #include <stdint.h>
 #include <string.h>
 #include "../../http_header_parser/http_header_structures.h"
+#include "../to_lower_case/to_lower_case.h"
 
-Header *find_header(Header **headers, uint8_t headersLength, const char *key)
+Header *find_header(Header **headers, uint8_t headersLength, char *key)
 {
+    to_lower_case(key);
     for (; headersLength--; *(headers++))
-        if (strcmp(tolower((*headers)->key), tolower(key)) == 0)
+        if (strcmp((*headers)->key, key) == 0)
             return *headers;
     return NULL;
 }
